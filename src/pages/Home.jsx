a3b2d56b3701792579cd9c88b42c7f516d93729b
nicoamaya3 <nicolasatehortuaamaya@gmail.com>
2026-03-27 11:32:41 -0500

    ajhikwd

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,11 +2,17 @@
 export const Home = () => {
   return (
     <>
-
-      <div className="container">
-        <div className="row">
-          <div className="col">
-            <h1>AHdh</h1>
+      <div style={{backgroundImage: "url(./cuervo.jpg)",
+        backgroundRepeat: "no-repeat"
+      }}>
+        <div className="container">
+          <div className="row">
+            <div className="col p-5">
+              <div className="d-flex flex-column align-items-center gap-5 p-5">
+                <img className="w-25" src="Logooo.png" alt="" />
+                <p className="w-50 fs-5 text-center text-white">Reconecta con el placer de la lectura a través de cuentos ilustrados e interactivos basados en las obras maestras de Edgar Allan Poe</p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
