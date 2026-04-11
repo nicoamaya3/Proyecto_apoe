738b2a6792692de6d2d9a3fed3e4c5b8ce8f3fb7
nicoamaya3 <nicolasatehortuaamaya@gmail.com>
2026-04-11 00:52:12 -0500

    LinksHechos

--- src/pages/Cuentogato.jsx
+++ src/pages/Cuentogato.jsx
@@ -6,7 +6,7 @@ export const Cuentogato = () => {
     <>
 
       <Cuentoc fon="url(./gatoN.jpg)" cat="Terror Psicológico
-       " titu="EL GATO NEGRO" sinop="Frank, un niño conocido por su bondad y amor por los animales en Boston, se convierte en un hombre monstruoso consumido por el alcohol. Su primer acto de maldad es arrancar un ojo a su querido gato negro Plutón, y más tarde lo ahorca. Un segundo gato aparece con una mancha en forma de horca, y el odio crece hasta que asesina a su esposa Katherine. Oculta el cadáver tras una pared falsa, pero un escalofriante maullido del gato emparedado revela su terrible crimen." img1="./fra.png" img2="./cat.png" img3="./zkat.png" linkk= "/LecturaG" txbtn="Empezar a leer" />
+       " titu="EL GATO NEGRO" sinop="Frank, un niño conocido por su bondad y amor por los animales en Boston, se convierte en un hombre monstruoso consumido por el alcohol. Su primer acto de maldad es arrancar un ojo a su querido gato negro Plutón, y más tarde lo ahorca. Un segundo gato aparece con una mancha en forma de horca, y el odio crece hasta que asesina a su esposa Katherine. Oculta el cadáver tras una pared falsa, pero un escalofriante maullido del gato emparedado revela su terrible crimen." img1="./fra.png" img2="./cat.png" img3="./kat.png" linkk= "/LecturaG" txbtn="Empezar a leer" />
 
     </>
   )
