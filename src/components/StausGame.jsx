0b71d157af4e75cd8a5859d3548b8d96d7b8ab47
Mariana218 <mcmariana218@gmail.com>
2026-04-30 11:30:13 -0500

    agg vidas

--- src/components/StausGame.jsx
+++ src/components/StausGame.jsx
@@ -12,6 +12,13 @@ export const StausGame = () => {
         <button>Gema 3</button>
       </div>
 
+
+      <div className="vidas">
+        <button>Vida 1</button>
+        <button>Vida 2</button>
+        <button>Vida 3</button>
+      </div>
+
     </>
   )
 }
