debb2c4a7d0926218c357827e9e32cc839dd390d
nicoamaya3 <nicolasatehortuaamaya@gmail.com>
2026-05-04 11:20:42 -0500

    cambios gemas

--- src/components/StausGame.jsx
+++ src/components/StausGame.jsx
@@ -3,13 +3,12 @@ import { useState } from "react"
 export const StausGame = () => {
   const [gemas, setGemas] = useState(0)
   const [vidas, setVidas] = useState(0)
-  const [character, setCharacter] = useState(0);
 
   return (
     <>
 
       <div className="gemas">
-        <h1>Gemas</h1>
+        <h1 style={{color : "white"}} >Gemas : {gemas} </h1>
         <button onClick={() => setGemas(prev => prev + 1)}>Gema 1</button>
         <button onClick={() => setGemas(prev => prev + 2)}>Gema 2</button>
         <button onClick={() => setGemas(prev => prev + 3)}>Gema 3</button>
