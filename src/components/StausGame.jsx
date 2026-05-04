819b0240d9871b85352dd2731c75b1cb303b8990
nicoamaya3 <nicolasatehortuaamaya@gmail.com>
2026-05-04 11:03:05 -0500

    Status

--- src/components/StausGame.jsx
+++ src/components/StausGame.jsx
@@ -1,12 +1,15 @@
 import { useState } from "react"
 
 export const StausGame = () => {
-  const [gemas, setGemas] = useState({})
-  const [vidas, setVidas] = useState({})
+  const [gemas, setGemas] = useState(0)
+  const [vidas, setVidas] = useState()
+  const [character, setCharacter] = useState(0);
+
   return (
     <>
 
       <div className="gemas">
+        <h1>Gemas</h1>
         <button>Gema 1</button>
         <button>Gema 2</button>
         <button>Gema 3</button>
@@ -14,6 +17,7 @@ export const StausGame = () => {
 
 
       <div className="vidas">
+        <h1>Vidas</h1>
         <button>Vida 1</button>
         <button>Vida 2</button>
         <button>Vida 3</button>
