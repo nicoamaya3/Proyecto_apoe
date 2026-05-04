0c4c09e1e92a801abe0d8694854e461e8dafd61c
nicoamaya3 <nicolasatehortuaamaya@gmail.com>
2026-05-04 10:42:40 -0500

    agg AppMain

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,11 @@ import './index.css'
 import { App } from './App.jsx'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap.min.js"
+import { AppMain } from './AppMain.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <AppMain/>
   </StrictMode>,
 )
