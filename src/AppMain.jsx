f58b89d311788ea1c0fd13b8dda9fcfe9fbc497c
Mariana218 <mcmariana218@gmail.com>
2026-05-06 11:06:14 -0500

    actualización

--- src/AppMain.jsx
+++ src/AppMain.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { StausGame } from './components/StausGame'
+import { MarianaRMPage } from './components/MarianaRMPage'
 
 export const AppMain = () => {
   return (
     <>
     
       <StausGame />
+      <MarianaRMPage/>
 
     </>
   )
