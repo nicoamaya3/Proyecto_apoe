b42bb9f4c0ab0482ecd8026dafd0e8e4377199dc
nicoamaya3 <nicolasatehortuaamaya@gmail.com>
2026-05-06 11:05:49 -0500

    pg nicoo

--- src/AppMain.jsx
+++ src/AppMain.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { StausGame } from './components/StausGame'
 import { AndresRMPage } from './pages/AndresRMPage'
+import { NicolasRMPages } from './pages/NicolasRMPages'
 
 export const AppMain = () => {
   return (
@@ -8,6 +9,8 @@ export const AppMain = () => {
     
       <StausGame />
       <AndresRMPage/>
+      <NicolasRMPages/>
+
 
     </>
   )
