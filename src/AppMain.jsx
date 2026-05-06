932fcf58ce513ef9ce6b5f24d573e0f73fb905a5
afsaanchez04 <afsanchez04@gmail.com>
2026-05-06 11:04:00 -0500

    pagina andres en appmain

--- src/AppMain.jsx
+++ src/AppMain.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { StausGame } from './components/StausGame'
+import { AndresRMPage } from './pages/AndresRMPage'
 
 export const AppMain = () => {
   return (
     <>
     
       <StausGame />
+      <AndresRMPage/>
 
     </>
   )
