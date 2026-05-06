14b8d9bc7b87971d2b5b51106979e1f3abb287d6
nicoamaya3 <nicolasatehortuaamaya@gmail.com>
2026-05-06 11:04:18 -0500

    Nicoo

--- src/pages/NicolasRMPages.jsx
+++ src/pages/NicolasRMPages.jsx
@@ -1,5 +1,23 @@
+import { useEffect, useState } from "react"
 
 export const NicolasRMPages = () => {
+  const [character, setCharacter] = useState([])
+
+  const getCharacter = async () => {
+
+    const res = await fetch("https://rickandmortyapi.com/api/character")
+    const data = res.json()
+
+    console.log(data)
+
+  }
+
+  useEffect(() => {
+    getCharacter()
+  }, [])
+  ()
+
+
   return (
     <>
     
