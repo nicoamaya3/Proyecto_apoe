90d9415956cbbd959c74297e838f4ce65f4a2291
Mariana218 <mcmariana218@gmail.com>
2026-05-07 10:50:38 -0500

    card

--- src/components/MarianaRMPage.jsx
+++ src/components/MarianaRMPage.jsx
@@ -25,13 +25,21 @@ export const MarianaRMPage = () => {
     <>
     
     <h1 className=' m-5 text-success  text-3xl fw-bold'>PERSONAJES DE RICK AND MORTY</h1>
-    <ul>
+    
       {characters.map((char, index) => (
-        <li key={index}>
-            <p>{char.name}</p>
-        </li>
+    <div key={index} className="card m-5 p-1" style={{ width: '18rem' }}>
+      <img src={char.image} className="card-img-top" alt=""/>
+      <div className="card-body">
+      <h5 className="card-title">{char.name}</h5>
+      <p className="card-text">Status: {char.status}</p>
+      <p className="card-text">Species: {char.species}</p>
+      <p className="card-text">Gender: {char.gender}</p>
+      </div>
+    </div>
       ))}
-    </ul>
+    
+
+    
 
     </>
 )
