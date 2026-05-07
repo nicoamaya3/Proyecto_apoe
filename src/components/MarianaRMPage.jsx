63b79b527b26765ec83966cd81b59c77d1123b73
Mariana218 <mcmariana218@gmail.com>
2026-05-07 11:24:55 -0500

    pokemones mariana

--- src/components/MarianaRMPage.jsx
+++ src/components/MarianaRMPage.jsx
@@ -20,10 +20,26 @@ export const MarianaRMPage = () => {
         getCharacters()
     }, [])
     
+    const [pokemons, setPokemons] = useState([])
+    
+    const getPokemons = async () => {
+
+        const res  = await fetch("https://pokeapi.co/api/v2/pokemon")
+        const data = await res.json()
+
+        setPokemons(data.results)
+
+        console.log(data)
+    }
+
+    useEffect(() => {   
+        getPokemons()
+    }, [])
 
   return (
     <>
-    
+    <div className='container d-flex flex-wrap'>
+
     <h1 className=' m-5 text-success  text-3xl fw-bold'>PERSONAJES DE RICK AND MORTY</h1>
     
       {characters.map((char, index) => (
@@ -38,9 +54,25 @@ export const MarianaRMPage = () => {
     </div>
       ))}
     
+    </div>
 
-    
 
+    <div className="container d-flex flex-wrap">
+
+    <h1 className=' m-5 text-success text-center text-3xl fw-bold'>POKEMONES</h1>
+
+     {pokemons.map((poke, index) => (
+       <div key={index} className="card m-5 p-1" style={{ width: '18rem' }}>
+      <img src={poke.url} className="card-img-top" alt=""/>
+      <div className="card-body">
+      <h5 className="card-title">{poke.name}</h5>
+      <p className="card-text">Weight: {poke.weight}</p>
+      </div>
+    </div>
+      ))}
+
+      </div>
+      
     </>
 )
 }
