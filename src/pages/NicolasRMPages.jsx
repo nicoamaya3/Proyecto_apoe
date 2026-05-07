4ffe38b0e7f095fd1f8f2d13f93f17d26ea00895
nicoamaya3 <nicolasatehortuaamaya@gmail.com>
2026-05-07 10:31:16 -0500

    carta n

--- src/pages/NicolasRMPages.jsx
+++ src/pages/NicolasRMPages.jsx
@@ -24,12 +24,21 @@ export const NicolasRMPages = () => {
       <h1>Personajes de Rick and  Morty </h1>
 
       <ul>
-        {character.map( (char, index ) => (
+        {character.map((char, index) => (
           <li key={index} >
             <p> {char.name} </p>
           </li>
-        ) )}
+        ))}
       </ul>
+
+      <div className="card" style={{width: "18rem;"}}>
+        <img src="..." className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+          <a href="#" className="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
 
     </>
   )
