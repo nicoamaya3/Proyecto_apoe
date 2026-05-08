6b09417fbca8bb4d6dbfa6aa69273ee17c28af63
Mariana218 <mcmariana218@gmail.com>
2026-05-08 11:27:56 -0500

    parrafo

--- src/components/MarianaRMPage.jsx
+++ src/components/MarianaRMPage.jsx
@@ -3,90 +3,94 @@ import { useEffect } from 'react'
 import { useState } from 'react'
 
 export const MarianaRMPage = () => {
-    
-    const [characters, setCharacters] = useState([])
-    
-    const getCharacters = async () => {
 
-        const res  = await fetch("https://rickandmortyapi.com/api/character")
-        const data = await res.json()
+  const [characters, setCharacters] = useState([])
 
-        setCharacters(data.results)
+  const getCharacters = async () => {
 
-        console.log(data)
-    }
+    const res = await fetch("https://rickandmortyapi.com/api/character")
+    const data = await res.json()
 
-    useEffect(() => {   
-        getCharacters()
-    }, [])
-    
-    const [pokemon, setPokemon] = useState([])
-    
-    const getPokemon = async () => {
+    setCharacters(data.results)
 
-        const res  = await fetch("https://pokeapi.co/api/v2/pokemon")
-        const data = await res.json()
+    console.log(data)
+  }
 
-        const detallesPokemon = await Promise.all(
+  useEffect(() => {
+    getCharacters()
+  }, [])
 
-            data.results.map(async (pokemon) => {
-              const res = await fetch(pokemon.url)
-              const detalles = await res.json()
+  const [pokemon, setPokemon] = useState([])
 
-              return {
-                id: detalles.id,
-                name: detalles.name,
-                image: detalles.sprites.other["official-artwork"].front_default,
-                type: detalles.types[0].type.name,
-              }
-            })
+  const getPokemon = async () => {
 
-        )
+    const res = await fetch("https://pokeapi.co/api/v2/pokemon?limit=12")
+    const data = await res.json()
 
-        
-        setPokemon(detallesPokemon)
-        console.log(data)
-    }
+    const detallesPokemon = await Promise.all(
+
+      data.results.map(async (pokemon) => {
+        const res = await fetch(pokemon.url)
+        const detalles = await res.json()
+
+        return {
+          id: detalles.id,
+          name: detalles.name,
+          image: detalles.sprites.other["official-artwork"].front_default,
+          type: detalles.types[0].type.name,
+        }
+      })
+
+    )
+
+
+    setPokemon(detallesPokemon)
+    console.log(data)
+  }
 
 
 
   return (
     <>
-    <div className='container d-flex flex-wrap'>
-
-    <h1 className=' m-5 text-success  text-3xl fw-bold'>PERSONAJES DE RICK AND MORTY (Mariana)</h1>
-    
-      {characters.map((char, index) => (
-    <div key={index} className="card m-5 p-1" style={{ width: '18rem' }}>
-      <img src={char.image} className="card-img-top" alt=""/>
-      <div className="card-body">
-      <h5 className="card-title">{char.name}</h5>
-      <p className="card-text">Status: {char.status}</p>
-      <p className="card-text">Species: {char.species}</p>
-      <p className="card-text">Gender: {char.gender}</p>
-      </div>
-    </div>
-      ))}
-    
-    </div>
+      <div className='container'>
+        <div className="row">
 
 
-    <div className="container d-flex flex-wrap">
+          <h1 className=' m-5 text-success  text-3xl fw-bold'>PERSONAJES DE RICK AND MORTY (Mariana)</h1>
 
-    <h1 className=' m-5 text-success text-center text-3xl fw-bold'>POKEMONES (Mariana)</h1>
-
-     {pokemon.map((poke, index) => (
-       <div key={index} className="card m-5 p-1" style={{ width: '18rem' }}>
-      <img src={poke.image} className="card-img-top" alt=""/>
-      <div className="card-body">
-      <h5 className="card-title">{poke.name}</h5>
-      <p className="card-text">Type: {poke.type}</p>
-      </div>
-    </div>
-      ))}
+          {characters.map((char, index) => (
+            <div key={index} className="col-6 card m-5 p-1" style={{ width: '18rem' }}>
+              <img src={char.image} className="card-img-top" alt="" />
+              <div className="card-body">
+                <h5 className="card-title">{char.name}</h5>
+                <p className="card-text">Status: {char.status}</p>
+                <p className="card-text">Species: {char.species}</p>
+                <p className="card-text">Gender: {char.gender}</p>
+              </div>
+            </div>
+          ))}
+        </div>
 
       </div>
-      
+
+
+      <div className="container d-flex flex-wrap">
+
+        <h1 className=' m-5 text-success text-center text-3xl fw-bold'>POKEMONES (Mariana)</h1>
+        <p>{pokemon}</p>
+
+        {pokemon.map((poke, index) => (
+          <div key={index} className="card m-5 p-1" style={{ width: '18rem' }}>
+            <img src={poke.image} className="card-img-top" alt="" />
+            <div className="card-body">
+              <h5 className="card-title">{poke.name}</h5>
+              <p className="card-text">Type: {poke.type}</p>
+            </div>
+          </div>
+        ))}
+
+      </div>
+
     </>
-)
+  )
 }
