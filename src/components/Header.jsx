58209709320c694f7240f5a33d948151a2e26877
Mariana218 <mcmaraiana218@gmail.com>
2026-05-19 11:31:13 -0500

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,49 +6,87 @@ export const Header = () => {
     return (
         <>
 
-            <nav className="navbar navbar-dark bg-black fixed-top shadow-sm">
-                <div className="container d-flex justify-content-between justify-content-md-start gap-md-5">
-                    <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
+        <div className="bg-black barra rounded-bottom-5 ">
+        <p className="bg-black text-white fontCard fs-5 text-center pt-2">Apoe!</p>
+        </div>
+
+            <div className="sidebar-container position-fixed top-0 start-0 d-flex align-items-center justify-content-center h-100 px-3">
+                <div className="custom-sidebar d-flex flex-column align-items-center justify-content-between py-4">
+
+                    <a href="#" className="sidebar-icon">
+                        
+                        <a href="#">
+                            <span className="icon">
+                                <ion-icon name="home-outline"></ion-icon>
+                            </span>
+                        </a>
+
+                        {/* <i className="bi bi-house-door-fill"><img src="iconos/Recurso1.svg" alt="" /></i> */} </a>
+
+                    <a href="#" className="sidebar-icon">
+                        <span className="icon-cat">🐈‍⬛</span>
+                    </a>
+
+                    <a href="#" className="sidebar-icon">
+                        <span className="icon-heart">🫀</span>
+                    </a>
+
+                    <a href="#" className="sidebar-icon">
+                        <span className="icon-barrel">☠️</span>
+                    </a>
+
+                    <a href="#" className="sidebar-icon">
+                        <span className="icon-handcuffs">⛓️</span>
+                    </a>
+
+                </div>
+            </div>
+
+
+
+            {/* <nav classNameName="navbar navbar-dark bg-black fixed-top shadow-sm">
+                <div classNameName="container d-flex justify-content-between justify-content-md-start gap-md-5">
+                    <button classNameName="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
+                        <span classNameName="navbar-toggler-icon"></span>
                     </button>
-                    <Link to='/' className="parrafo navbar-brand fontCard" href="#">Apoe!</Link>
+                    <Link to='/' classNameName="parrafo navbar-brand fontCard" href="#">Apoe!</Link>
 
                     <div style={{
                         background: 'linear-gradient(135deg, #1d0b0b 0%, #0d0d0d 100%)', borderRadius: '15px'
-                    }} className="offcanvas offcanvas-start text-bg-dark text-center" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
-                        <div style={{ background: 'transparent' }} className="offcanvas-header justify-content-end">
+                    }} classNameName="offcanvas offcanvas-start text-bg-dark text-center" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
+                        <div style={{ background: 'transparent' }} classNameName="offcanvas-header justify-content-end">
 
-                            <button type="button" className="btn-close btn-close-white mb-5 me-1" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                            <button type="button" classNameName="btn-close btn-close-white mb-5 me-1" data-bs-dismiss="offcanvas" aria-label="Close"></button>
 
                         </div>
-                        <div className='logo bg-white rounded-circle p-2 p-md-3 mx-auto'>
+                        <div classNameName='logo bg-white rounded-circle p-2 p-md-3 mx-auto'>
                             <img src="/public/Logo.png" alt="" />
                         </div>
-                        <div style={{ background: 'transparent' }} className="offcanvas-body">
-                            <ul className="navbar-nav flex-grow-1 fs-5">
-                                <li className="nav-item mb-2">
-                                    <Link to='/' className="nav-link active" aria-current="page" href="#">✪ Inicio</Link>
+                        <div style={{ background: 'transparent' }} classNameName="offcanvas-body">
+                            <ul classNameName="navbar-nav flex-grow-1 fs-5">
+                                <li classNameName="nav-item mb-2">
+                                    <Link to='/' classNameName="nav-link active" aria-current="page" href="#">✪ Inicio</Link>
                                 </li>
-                                <li className="nav-item dropdown mb-2">
-                                    <Link to='/Cuentos' className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <li classNameName="nav-item dropdown mb-2">
+                                    <Link to='/Cuentos' classNameName="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                         ✒ Cuentos
                                     </Link>
-                                    <ul className="dropdown-menu dropdown-menu-dark bg-black border-secondary">
-                                        <li><Link to='/Gatonegro' className="dropdown-item" href="#">🐈‍⬛ Gato Negro</Link></li>
-                                        <li><Link to='Delator' className="dropdown-item"
+                                    <ul classNameName="dropdown-menu dropdown-menu-dark bg-black border-secondary">
+                                        <li><Link to='/Gatonegro' classNameName="dropdown-item" href="#">🐈‍⬛ Gato Negro</Link></li>
+                                        <li><Link to='Delator' classNameName="dropdown-item"
                                             href="#">🫀 Corazón Delator</Link></li>
                                     </ul>
                                 </li>
-                                <li className="nav-item">
-                                    <Link to='Nosotros' className="nav-link" href="#">⍟ Nosotros</Link>
+                                <li classNameName="nav-item">
+                                    <Link to='Nosotros' classNameName="nav-link" href="#">⍟ Nosotros</Link>
                                 </li>
                             </ul>
 
                         </div>
-                        <p className="parrafo mt-auto small opacity-50 px-3">© 2026 Apoe! - Todos los derechos reservados.</p>
+                        <p classNameName="parrafo mt-auto small opacity-50 px-3">© 2026 Apoe! - Todos los derechos reservados.</p>
                     </div>
                 </div>
-            </nav>
+            </nav> */}
 
         </>
     )
