34f2794ad73c48695ee02b38308e02df0daaa755
Mariana218 <mcmariana218@gmail.com>
2026-05-27 11:23:51 -0500

    ejemplo

--- src/components/ReproductorMari.jsx
+++ src/components/ReproductorMari.jsx
@@ -1,21 +1,72 @@
-import React from 'react'
+import { div, video } from 'motion/react-client'
+import React, { useRef } from 'react'
 
 export const ReproductorMari = () => {
+
+    const [mostrarMensaje, setMostrarMensaje] = useState(false)
+    const [detener, setDetener] = useState(false)
+
+    const videoRef = useRef (null)
+
+    const handlePlay = () => {
+        videoRef.current.play()
+    }
+    
+    const handlePause = () => {
+        videoRef.current.pause()
+    }
+
+    const handleAvanzar = () => {
+        videoRef.current.currentTime = 6
+    }
+
+    const handleUpdate = () => {
+        if( videoRef.current.currentTime >= 5 && !detener){
+            videoRef.current.pause()
+            setMostrarMensaje(true)
+            setDetener(true)
+        }
+    }
+
+    const handleContinuar = () => {
+        setMostrarMensaje(false)
+        videoRef.current.play()
+    }
+
+
+
   return (
     <>
 
     <h1>Reproductor de Mariana</h1>
 
-    <video width={700}>
+    <video 
+        ref = {videoRef}
+        width={700}
+        oneTimeUpdate={handleUpdate}
+        >
+
         <source 
         src='https://www.w3schools.com/Html/mov_bbb.mp4'
         type='Video/mp4' 
-        />
+
+    />
     </video>
 
+    {
+        mostrarMensaje && (
+            <div>
+                <h2>Video detenido en el Seg 5</h2>
+            </div>
+        )
+    }
+
     <div>
-        <button>Play</button>
-        <button>Pause</button>
+        <button onClick={handlePlay}>Play</button>
+        <button onClick={handlePause}>Pause</button>
+        <button onClick={handleAvanzar}>Ir al Seg 6</button>
+        <button onClick={handleContinuar}>Continuar</button>
+
     </div>
 
     </>
