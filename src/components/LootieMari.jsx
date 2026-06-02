6819f7c972d693373b85b2df3f8c7d11d2e68ec7
Mariana218 <mcmariana218@gmail.com>
2026-06-02 11:15:06 -0500

    actualización

--- src/components/LootieMari.jsx
+++ src/components/LootieMari.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import Lottie from "lottie-react"
+import ejercicioAnimation from "../assets/ejercicio.json"
 
 export const LootieMari = () => {
   return (
-    <div>LootieMari</div>
+    <div>
+      <Lottie 
+      animationData={ejercicioAnimation} 
+      loop={true}
+      style={{ width: 500, height: 500 }}
+      />
+    </div>
   )
 }
