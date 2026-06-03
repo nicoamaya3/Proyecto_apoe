e72a2ce768c6fb4f70fa9b0b4a7a5f2b267dca8d
Mariana218 <mcmariana218@gmail.com>
2026-06-03 11:04:28 -0500

    pelota

--- src/components/LootieMari.jsx
+++ src/components/LootieMari.jsx
@@ -1,12 +1,25 @@
-import Lottie from "lottie-react"
+import LottieModule from "lottie-react"
 import ejercicioAnimation from "../assets/ejercicio.json"
+import { useRef } from "react";
+
+const Lottie = LottieModule.default;
 
 export const LootieMari = () => {
+
+  const lottieRef = useRef()
+
+  const reproducir = () => {
+    lottieRef.current.stop()
+    lottieRef.current.play()
+  }
+
   return (
-    <div>
+    <div onClick={reproducir}>
       <Lottie 
+      lottieRef={lottieRef}
       animationData={ejercicioAnimation} 
       loop={true}
+      autoplay={false}
       style={{ width: 500, height: 500 }}
       />
     </div>
