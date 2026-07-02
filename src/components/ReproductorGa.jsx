813c1a22c99ab1e1fbafc01829eb7d8d2eb2994c
Hillary856 <hillary3247@gmail.com>
2026-07-02 11:46:45 -0500

    arbusto

--- src/components/ReproductorGa.jsx
+++ src/components/ReproductorGa.jsx
@@ -1,17 +1,32 @@
-import React from 'react'
+import { useRef } from "react"
 import LottieModule from "lottie-react";
+import arbustosdel from "../assets/arbustosdel.json"
 import "../stylesheets/AnimacionGato.css"
+
+const Lottie = LottieModule.default;
 
 
 export const ReproductorGa = () => {
+
+  const lottieRefarbustosdel = useRef()
+
+  const reproducirarbustosdel = () => {
+        lottieRefarbustosdel.current.stop()
+        lottieRefarbustosdel.current.play()
+  }
+
+
   return (
     <>
-    <div className="container py-5">
-      <div className="row justify-content-center">
-        <div className="col col-6 bg-light text-center">
-          <img className="escenario" src="escenario.webp" alt="" />
-
-        </div>
+    <div className="escenario py-5">
+      <img className='exterior' src="escenario.webp" alt="" />
+      <div>
+        <Lottie onClick={reproducirarbustosdel} className="arbustosdel"
+          lottieRef={lottieRefarbustosdel}
+          animationData={luz}
+          loop={false}
+          autoplay={false}
+        />
       </div>
 
     </div>
