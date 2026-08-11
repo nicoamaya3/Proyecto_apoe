3e52655dc1adcc752cb9d4ce8de9a5296ef293cd
nicoamaya3 <nicolasatehortuaamaya@gmail.com>
2026-08-11 07:27:34 -0500

    caps

---
+++ src/components/Cap1corazon.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../stylesheets/AnimacionCorazon.css";
+
+export const Cap1corazon = () => {
+  return (
+
+    <>
+    <video className='pantalla' src="./public/Videos/Corazón/Cap1.mp4"></video>
+    </>
+
+  )
+}
