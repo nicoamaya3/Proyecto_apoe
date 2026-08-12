04899e786c1eba82a6deeaab7f5d2a669cbe526f
nicoamaya3 <nicolasatehortuaamaya@gmail.com>
2026-08-12 09:27:56 -0500

    cambios

--- src/components/PortadaCorazon.jsx
+++ src/components/PortadaCorazon.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import "../stylesheets/AnimacionCorazon.css";
+
 
 export const PortadaCorazon = () => {
   return (
-    <div>PortadaCorazon</div>
+    <>
+      <div className='pantalla'>
+        <img src="./public/portadasCorazon/portadacap2.svg" alt="" />
+      </div>
+    </>
   )
 }
