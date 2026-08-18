809117a86d77aff87bb43b19f5a8956ba3305c09
nicoamaya3 <nicolasatehortuaamaya@gmail.com>
2026-08-18 11:06:20 -0500

    Portada

--- src/components/Portadacorazon1.jsx
+++ src/components/Portadacorazon1.jsx
@@ -7,7 +7,7 @@ export const Portadacorazon1 = () => {
     <>
     
       <div className='pantalla'>
-        <img src="./public/portadasCorazon/portadacap1.svg" alt="" />
+        <img src="./portadacap1.svg" alt="" />
       </div>
 
     </>
